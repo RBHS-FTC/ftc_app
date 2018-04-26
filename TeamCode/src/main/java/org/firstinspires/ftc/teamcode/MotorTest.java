e7bf7ff9d670b8c7f2b6da26c1bf7d7ca23284e4
Scott Lagler <laglerscott@gmail.com>
2018-04-26 18:26:19 -0500

    Add Servo test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -12,10 +12,7 @@ import junit.framework.Test;
 
 @TeleOp
 public class MotorTest extends LinearOpMode {
-    private Gyroscope imu;
     private DcMotor motorTest;
-    private DigitalChannel digitalTouch;
-    private DistanceSensor sensorColorRange;
     private Servo servoTest;
 
     @Override
@@ -28,13 +25,13 @@ public class MotorTest extends LinearOpMode {
 
         waitForStart();
 
+        // run until the end of the match (driver presses STOP)
         double tgtPower = 0;
         while (opModeIsActive()) {
-            /*
             tgtPower = -this.gamepad1.left_stick_y;
             motorTest.setPower(tgtPower);
             // check to see if we need to move the servo.
-            if (gamepad1.y) {
+            if(gamepad1.y) {
                 // move to 0 degrees.
                 servoTest.setPosition(0);
             } else if (gamepad1.x || gamepad1.b) {
@@ -42,21 +39,27 @@ public class MotorTest extends LinearOpMode {
                 servoTest.setPosition(0.5);
             } else if (gamepad1.a) {
                 // move to 180 degrees.
-                servoTest.setPosition(1);
+                // Servo stuck at 180 degrees
+                servoTest.setPosition(0.95);
             }
             telemetry.addData("Servo Position", servoTest.getPosition());
             telemetry.addData("Target Power", tgtPower);
             telemetry.addData("Motor Power", motorTest.getPower());
             telemetry.addData("Status", "Running");
             telemetry.update();
-            */
-            tgtPower = 1; //-this.gamepad1.left_stick_y;
+
+        }
+        /*
+        double tgtPower = 0;
+        while (opModeIsActive()) {
+            tgtPower = -this.gamepad1.left_stick_y;
             motorTest.setPower(tgtPower);
             telemetry.addData("Target Power", tgtPower);
             telemetry.addData("servoTest", motorTest.getPower());
             telemetry.addData("Status", "Initialized");
             telemetry.update();
         }
+        */
 
     }
 }
