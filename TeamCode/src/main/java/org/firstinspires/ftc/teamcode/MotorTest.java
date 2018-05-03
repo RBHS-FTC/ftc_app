d31655af1345707617f6e22c36b785ee5f93d28e
Scott Lagler <laglerscott@gmail.com>
2018-05-03 15:17:31 -0500

    Fixed MotorTest not being read

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+@TeleOp
 public class MotorTest extends LinearOpMode {
 	private DcMotor MotorTest;
 
