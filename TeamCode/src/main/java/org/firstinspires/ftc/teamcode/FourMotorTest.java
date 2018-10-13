e62f781eba3ebd0abc550b1c680c81f205771a78
Scott Lagler <laglerscott@gmail.com>
2018-10-13 17:26:31 -0500

    Fixed names for motors closes #6

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourMotorTest.java
@@ -6,17 +6,17 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class FourMotorTest extends LinearOpMode{
+    private DcMotor Motor0;
     private DcMotor Motor1;
     private DcMotor Motor2;
     private DcMotor Motor3;
-    private DcMotor Motor4;
 
     @Override
     public void runOpMode() {
-        Motor1 = hardwareMap.get(DcMotor.class, "Motor1");
-        Motor2 = hardwareMap.get(DcMotor.class, "Motor2");
-        Motor3 = hardwareMap.get(DcMotor.class, "Motor3");
-        Motor4 = hardwareMap.get(DcMotor.class, "Motor4");
+        Motor0 = hardwareMap.get(DcMotor.class, "Motor1");
+        Motor1 = hardwareMap.get(DcMotor.class, "Motor2");
+        Motor2 = hardwareMap.get(DcMotor.class, "Motor3");
+        Motor3 = hardwareMap.get(DcMotor.class, "Motor4");
 
         telemetry.addData("Status", "Initalized");
         telemetry.update();
@@ -28,10 +28,10 @@ public class FourMotorTest extends LinearOpMode{
         while (opModeIsActive()) {
             conPwrX = -this.gamepad1.left_stick_x;
             conPwrY = -this.gamepad1.left_stick_y;
-            Motor1.setPower(-conPwrY);
+            Motor0.setPower(-conPwrY);
+            Motor1.setPower(conPwrY);
             Motor2.setPower(conPwrY);
-            Motor3.setPower(conPwrY);
-            Motor4.setPower(-conPwrY);
+            Motor3.setPower(-conPwrY);
 
             /*
             Diagonal Strafe
@@ -47,10 +47,10 @@ public class FourMotorTest extends LinearOpMode{
             M4 = -
              */
             telemetry.addData("Target Power", conPwrY);
-            telemetry.addData("Motor1", Motor1.getPower());
-            telemetry.addData("Motor2", Motor2.getPower());
-            telemetry.addData("Motor3", Motor3.getPower());
-            telemetry.addData("Motor4", Motor4.getPower());
+            telemetry.addData("Motor1", Motor0.getPower());
+            telemetry.addData("Motor2", Motor1.getPower());
+            telemetry.addData("Motor3", Motor2.getPower());
+            telemetry.addData("Motor4", Motor3.getPower());
             telemetry.update();
         }
     }
