d47491431481594bdb1686e39885ab9f6d63306b
Scott Lagler <laglerscott@gmail.com>
2018-11-09 15:32:31 -0600

    Fix motor names and add lock servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoControllerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoControllerTest.java
@@ -13,7 +13,10 @@ public class TwoControllerTest extends LinearOpMode {
     private DcMotor Motor3;
     private DcMotor Motor4;
     private DcMotor Motor5;
+    private DcMotor Motor6;
+    private DcMotor Motor7;
     private Servo Servo0;
+    private Servo Servo1;
 
     @Override
     public void runOpMode() {
@@ -22,9 +25,12 @@ public class TwoControllerTest extends LinearOpMode {
         Motor2 = hardwareMap.get(DcMotor.class, "Motor2");
         Motor3 = hardwareMap.get(DcMotor.class, "Motor3");
         Motor4 = hardwareMap.get(DcMotor.class, "Motor4");
-        Motor5 = hardwareMap.get(DcMotor.class, "Motor6");
+        Motor5 = hardwareMap.get(DcMotor.class, "Motor5");
+        Motor6 = hardwareMap.get(DcMotor.class, "Motor6");
+        Motor7 = hardwareMap.get(DcMotor.class, "Motor7");
 
         Servo0 = hardwareMap.get(Servo.class, "Servo0");
+        Servo1 = hardwareMap.get(Servo.class, "Servo1");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -34,7 +40,7 @@ public class TwoControllerTest extends LinearOpMode {
         double controllerOnePowerLeftY = 0;
         double controllerTwoPowerRightY = 0;
         while (opModeIsActive()) {
-            controllerOnePowerLeftY = -this.gamepad1.left_stick_y;
+            controllerOnePowerLeftY = -this.gamepad2.left_stick_y;
             controllerTwoPowerRightY = -this.gamepad2.right_stick_y;
             /*
             //Forwards and Backwards
@@ -49,9 +55,12 @@ public class TwoControllerTest extends LinearOpMode {
 
             //Motor to move arm up and down
             Motor5.setPower(controllerTwoPowerRightY);
+
             if (gamepad2.b) Servo0.setPosition(0.5);
-            if (gamepad2.a) Motor4.setPower(1);
-            if (gamepad2.y) Motor5.setPower(-1);
+            if (gamepad2.a) Motor6.setPower(1); //Motor to collect balls
+            if (gamepad2.y) Motor7.setPower(-1); // Drop collection Arm
+            if (gamepad2.x && Servo1.getPosition() == 90 ) Servo1.setPosition(0);
+            if (gamepad2.x && Servo1.getPosition() == 0 ) Servo1.setPosition(90);
 
             telemetry.addData("Motor0", Motor0.getPower());
             telemetry.addData("Motor1", Motor1.getPower());
